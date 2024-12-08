6b1a926a364838cf1a66aa7776b9f0541c427aaf
layanhad <layanosh3@gmail.com>
2024-12-08 22:20:51 +0200

    authorization using clerk

--- harmony-headlines/src/pages/ArticlePage/ArticlePage.jsx
+++ harmony-headlines/src/pages/ArticlePage/ArticlePage.jsx
@@ -3,11 +3,14 @@ import { useParams,Link } from "react-router-dom";
 import  newsData  from "../../data/newsData";
 import { useContext } from "react";
 import { NewsContext } from "../../context/NewsContext";
+import {useUser} from "@clerk/clerk-react"
 
 export default function ArticlePage() {
     const { id } = useParams();
     const { news, updateArticleMood } = useContext(NewsContext);
     const article = news.find((newsItem) => newsItem.id === parseInt(id));
+    const user = useUser();
+    const isAdmin = user.user?.publicMetadata.role === "admin";
 
     if (!article) return <p>Article not found.</p>;
 
@@ -24,10 +27,10 @@ export default function ArticlePage() {
                 </div>
                 <p className="article-source">Source: {article.source}</p>
                 <p className="article-description">{article.description}</p>
-                <p className="articale-score">sentimentScore: {article.sentimentScore}</p>
-                <button className="score-button" onClick={() => updateArticleMood(article.id)}>
+                <p className="articale-score">Mood: {article.sentimentScore}</p>
+                {isAdmin && <button className="score-button" onClick={() => updateArticleMood(article.id)}>
                     Make Article Kinder
-                </button>
+                </button>}
                 <br></br>
                 <Link to="/news">
                     <button className="back-button">Go back</button>
